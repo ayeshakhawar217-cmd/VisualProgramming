196d2655f609c89f585c20c1567f5f698ccf445e
ayesha khawar <ayeshakhawar217@gmail.com>
2025-10-04 14:28:30 +0500

    Object added

--- VisualProgramming/Program.cs
+++ VisualProgramming/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! C#");
-Console.WriteLine("Welcome GitHub, Another Modification");
+﻿using System;
+using VisualProgramming;
+
+class Program
+{
+   
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Enter Number");
+   int num = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine(i * i * i);
+    }
+
+        Class1 obj = new Class1();
+        obj.display();
+}
+
+}
+
